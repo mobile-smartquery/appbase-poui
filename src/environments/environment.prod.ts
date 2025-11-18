af2e7e023b9a35036e0d00e0e3fabbdf1ebb9303
Gabriel Mariano da Silva <gabrielmarianodasilvaa2001@hotmail.com>
2025-11-18 11:32:31 -0300

    chore(env): add apiEndpointPath to production env to fix Vercel build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
 
+  apiEndpointPath: 'http://protheusawsmobile.ddns.net:8080/rest',
   apiBaseUrl: '/rest',
   oauthTokenUrl: '/rest/api/oauth2/v1/token',
 
